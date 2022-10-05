1248a981944b699512a3fb67d0db3707fdbd730b
Chris Li <chris.lty07@gmail.com>
2022-10-05 10:37:26 -0400

    Completed fibonacci challenge

--- javascript/fibonacci.js
+++ javascript/fibonacci.js
@@ -1,6 +1,19 @@
 function fibonacci(num) {
-  // type your code here
+  const fibArr = [0,1];
+
+  if(num === 0) {
+    return 0;
+  } else if (num === 1) {
+    return 1;
+  } else {
+    for(i = 2; i <= num; i++) {
+      fibArr.push(fibArr[i-1]+fibArr[i-2]);
+    }
+  }
+  return fibArr[fibArr.length-1];
 }
+
+console.log(fibonacci(3))
 
 if (require.main === module) {
   // add your own tests in here
